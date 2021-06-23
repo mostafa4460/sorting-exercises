d8c8e265213610482607c599eaf87050896f441a
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-06-23 00:35:31 -0400

    bubbleSort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,17 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        let swapped = false;
+        for (let j = 0; j < i; j++) {
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) return arr;
+    }
+    return arr;
+}
 
 module.exports = bubbleSort;
